8a904badc8a7ee5bbaf3b886e1b82e1335ed3346
Fábio Silva <fabio.fernando.osilva@gmail.com>
2021-04-13 08:31:15 -0300

    Finish FP.4a & FP.5a

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -14,32 +14,8 @@ template <class T>
 class MessageQueue
 {
 public:
-    T receive()
-    {
-        // perform queue modification under the lock
-        std::unique_lock<std::mutex> uLock(_mutex);
-        _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
-
-        // remove last vector element from queue
-        T msg = std::move(_queue.back());
-        _queue.pop_back();
-
-        return msg; // will not be copied due to return value optimization (RVO) in C++
-    }
-
-    void send(T &&msg)
-    {
-        // simulate some work
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
-
-        // perform vector modification under the lock
-        std::lock_guard<std::mutex> uLock(_mutex);
-
-        // add vector to queue
-        std::cout << "   Message " << msg << " has been sent to the queue" << std::endl;
-        _queue.push_back(std::move(msg));
-        _cond.notify_one(); // notify client after pushing new Vehicle into vector
-    }
+    T receive();
+    void send(T &&msg);
 
 private:
     std::mutex _mutex;
