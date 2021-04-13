206105b84a601adf09b7b199b1830f89b997c627
Fábio Silva <fabio.fernando.osilva@gmail.com>
2021-04-13 08:21:38 -0300

    FP.3: Define a class "MessageQueue" which has the public methods send and receive

--- src/TrafficLight.h
+++ src/TrafficLight.h
@@ -9,19 +9,41 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
-
-// FP.3 Define a class „MessageQueue“ which has the public methods send and receive. 
-// Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type. 
-// Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase. 
-// Also, there should be an std::condition_variable as well as an std::mutex as private members. 
-
 template <class T>
 class MessageQueue
 {
 public:
+    T receive()
+    {
+        // perform queue modification under the lock
+        std::unique_lock<std::mutex> uLock(_mutex);
+        _cond.wait(uLock, [this] { return !_queue.empty(); }); // pass unique lock to condition variable
+
+        // remove last vector element from queue
+        T msg = std::move(_queue.back());
+        _queue.pop_back();
+
+        return msg; // will not be copied due to return value optimization (RVO) in C++
+    }
+
+    void send(T &&msg)
+    {
+        // simulate some work
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+        // perform vector modification under the lock
+        std::lock_guard<std::mutex> uLock(_mutex);
+
+        // add vector to queue
+        std::cout << "   Message " << msg << " has been sent to the queue" << std::endl;
+        _queue.push_back(std::move(msg));
+        _cond.notify_one(); // notify client after pushing new Vehicle into vector
+    }
 
 private:
-    
+    std::mutex _mutex;
+    std::condition_variable _cond;
+    std::deque<T> _queue;
 };
 
 enum TrafficLightPhase{
